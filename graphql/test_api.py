8e31a74eb81fd0a2af7f6f819a169e9695ea32f0
Malachi Griffith <malachig@gmail.com>
2026-02-04 09:17:06 -0600

    update to use a method that constructs the graphql query

--- graphql/test_api.py
+++ graphql/test_api.py
@@ -1,20 +1,59 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import requests
-API_URL = "https://civicdb.org/api/graphql"
+import pdb
+
+api_url = "https://civicdb.org/api/graphql"
 
 # Read the JSON file into a string variable
-with open('variant_CoordinateIdsForVariant_query.json', 'r') as file:
-    payload = file.read()
-print(payload)
+#with open('variant_CoordinateIdsForVariant_query.json', 'r') as file:
+#    query = file.read()
 
-with open('variant_CoordinateIdsForVariant_variables.json', 'r') as file:
-    variables = file.read()
-print(variables)
+#with open('variant_CoordinateIdsForVariant_variables.json', 'r') as file:
+#    variables = file.read()
 
-resp = requests.post(
-   API_URL, json={"query": payload, "variables": variables}, timeout=(10, 200)
-)
+#resp = requests.post(
+#   api_url, json={"query": query, "variables": variables}, timeout=(10, 200)
+#)
 
-print(resp)
+def run_graphql_operation(api_url, operation_name, timeout=(10, 200)):
+    # Base directory = directory containing this script
+    base_dir = Path(__file__).resolve().parent
+
+    query_path = base_dir / f"{operation_name}_query.json"
+    variables_path = base_dir / f"{operation_name}_variables.json"
+
+    with query_path.open("r") as f:
+        query = f.read()
+
+    with variables_path.open("r") as f:
+        variables = f.read()
+
+    resp = requests.post(
+        api_url,
+        json={
+            "query": query,
+            "variables": variables
+        },
+        timeout=timeout
+    )
+
+    return resp
+
+#Get coordinate ids for variant
+operation_name = "variant_CoordinateIdsForVariant"
+resp = run_graphql_operation(api_url, operation_name)
+json = resp.json()
+json['data']
+open_revision_count_variant = json['data']['variant']['openRevisionCount']
+open_revision_count_coordinates = json['data']['variant']['coordinates']['openRevisionCount']
+
+print(open_revision_count_variant)
+print(open_revision_count_coordinates)
+
+#pdb.set_trace()
+#json = resp.json()
+
+
 
