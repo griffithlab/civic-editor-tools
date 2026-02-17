98305c822296e8257faf94eecbd25ebb8dc5ce13
Malachi Griffith <malachig@gmail.com>
2026-02-16 20:13:24 -0600

    minor update

--- data/refseq/get_refseq_protein.sh
+++ data/refseq/get_refseq_protein.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 wget https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/reference/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_protein.faa.gz
 
 
